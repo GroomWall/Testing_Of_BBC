f0bea06b0d119c1b9937edb96e4ceafc6db591a3
Vadym Kalmykov <kvvad@ukr.net>
2020-03-27 18:35:56 +0200

    Second push

--- src/test/java/testBBC/TestQuestionSubmit.java
+++ src/test/java/testBBC/TestQuestionSubmit.java
@@ -2,8 +2,6 @@ package testBBC;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import webDriverSettings.WebDriverSettings;
@@ -12,21 +10,20 @@ public class TestQuestionSubmit extends WebDriverSettings {
     @Test
     public void Aberdeen(){
         driver.get("https://www.bbc.com/");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")));
+        waiting("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]");
 
         driver.findElement(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")).click();
         driver.findElement(By.xpath("//a[@href='/sport/football'][@data-stat-name='primary-nav-v2']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'All Teams')]")));
+        waiting("//a[contains(text(),'All Teams')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'All Teams')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Aberdeen')]")));
+        waiting("//a[contains(text(),'Aberdeen')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'Aberdeen')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")));
+        waiting("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']");
 
         driver.findElement(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Hibernian')]")));
+        waiting("//span[contains(text(),'Hibernian')]");
 
         boolean teamOneDisplay = driver.findElement(By.xpath("//span[@class='sp-c-fixture__team-name sp-c-fixture__team-name--home']//span[@class='gs-u-display-none gs-u-display-block@m qa-full-team-name sp-c-fixture__team-name-trunc'][contains(text(),'Aberdeen')]")).getText().equals("Aberdeen");
         boolean teamTwoDisplay = driver.findElement(By.xpath("//span[contains(text(),'Hibernian')]")).getText().equals("Hibernian");
@@ -34,10 +31,10 @@ public class TestQuestionSubmit extends WebDriverSettings {
         boolean teamTwoScore = driver.findElement(By.xpath("//span[@class='sp-c-fixture__number sp-c-fixture__number--away sp-c-fixture__number--ft'][contains(text(),'1')]")).getText().equals("1");
 
         Assert.assertTrue((teamOneDisplay && teamTwoDisplay) && (teamOneScore && teamTwoScore));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/sport/football/51685369']")));
+        waiting("//a[@href='/sport/football/51685369']");
 
         driver.findElement(By.xpath("//a[@href='/sport/football/51685369']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']")));
+        waiting("//span[@class='fixture__number fixture__number--home fixture__number--ft']");
 
         String expectedScoreAberdeen = "3";
         WebElement actualScoreAberdeen = driver.findElement(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']"));
@@ -53,20 +50,20 @@ public class TestQuestionSubmit extends WebDriverSettings {
     @Test
     public void Celtic(){
         driver.get("https://www.bbc.com/");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")));
+        waiting("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]");
+
         driver.findElement(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")).click();
         driver.findElement(By.xpath("//a[@href='/sport/football'][@data-stat-name='primary-nav-v2']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'All Teams')]")));
+        waiting("//a[contains(text(),'All Teams')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'All Teams')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Celtic')]")));
+        waiting("//a[contains(text(),'Celtic')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'Celtic')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")));
+        waiting("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']");
 
         driver.findElement(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Celtic')]")));
+        waiting("//span[contains(text(),'Celtic')]");
 
         boolean teamOneDisplay = driver.findElement(By.xpath("//a[@class='sp-c-fixture__block-link']//span[@class='sp-c-fixture__team sp-c-fixture__team--home']//span[@class='gs-u-display-none gs-u-display-block@m qa-full-team-name sp-c-fixture__team-name-trunc'][contains(text(),'Celtic')]")).getText().equals("Celtic");
         boolean teamTwoDisplay = driver.findElement(By.xpath("//span[contains(text(),'St Mirren')]")).getText().equals("St Mirren");
@@ -74,10 +71,10 @@ public class TestQuestionSubmit extends WebDriverSettings {
         boolean teamTwoScore = driver.findElement(By.xpath("//span[@class='sp-c-fixture__number sp-c-fixture__number--away sp-c-fixture__number--ft'][contains(text(),'0')]")).getText().equals("0");
 
         Assert.assertTrue((teamOneDisplay && teamTwoDisplay) && (teamOneScore && teamTwoScore));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/sport/football/51685365']")));
+        waiting("//a[@href='/sport/football/51685365']");
 
         driver.findElement(By.xpath("//a[@href='/sport/football/51685365']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']")));
+        waiting("//span[@class='fixture__number fixture__number--home fixture__number--ft']");
 
         String expectedScoreAberdeen = "5";
         WebElement actualScoreAberdeen = driver.findElement(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']"));
@@ -93,21 +90,20 @@ public class TestQuestionSubmit extends WebDriverSettings {
     @Test
     public void HeartOfMidlothian(){
         driver.get("https://www.bbc.com/");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")));
+        waiting("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]");
 
         driver.findElement(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")).click();
         driver.findElement(By.xpath("//a[@href='/sport/football'][@data-stat-name='primary-nav-v2']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'All Teams')]")));
+        waiting("//a[contains(text(),'All Teams')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'All Teams')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Heart of Midlothian')]")));
+        waiting("//a[contains(text(),'Heart of Midlothian')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'Heart of Midlothian')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")));
+        waiting("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']");
 
         driver.findElement(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Heart of Midlothian')]")));
+        waiting("//span[contains(text(),'Heart of Midlothian')]");
 
         boolean teamOneDisplay = driver.findElement(By.xpath("//div[3]//span[3]//span[1]//span[1]//span[1]")).getText().equals("Heart of Midlothian");
         boolean teamTwoDisplay = driver.findElement(By.xpath("//span[contains(text(),'St Mirren')]")).getText().equals("St Mirren");
@@ -115,10 +111,10 @@ public class TestQuestionSubmit extends WebDriverSettings {
         boolean teamTwoScore = driver.findElement(By.xpath("//span[@class='sp-c-fixture__number sp-c-fixture__number--away sp-c-fixture__number--ft'][contains(text(),'0')]")).getText().equals("0");
 
         Assert.assertTrue((teamOneDisplay && teamTwoDisplay) && (teamOneScore && teamTwoScore));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/sport/football/51498748']")));
+        waiting("//a[@href='/sport/football/51498748']");
 
         driver.findElement(By.xpath("//a[@href='/sport/football/51498748']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']")));
+        waiting("//span[@class='fixture__number fixture__number--home fixture__number--ft']");
 
         String expectedScoreAberdeen = "1";
         WebElement actualScoreAberdeen = driver.findElement(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']"));
@@ -134,21 +130,20 @@ public class TestQuestionSubmit extends WebDriverSettings {
     @Test
     public void Livingston(){
         driver.get("https://www.bbc.com/");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")));
+        waiting("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]");
 
         driver.findElement(By.xpath("//div[@id='orb-nav-links']//a[contains(text(),'Sport')]")).click();
         driver.findElement(By.xpath("//a[@href='/sport/football'][@data-stat-name='primary-nav-v2']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'All Teams')]")));
+        waiting("//a[contains(text(),'All Teams')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'All Teams')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Livingston')]")));
+        waiting("//a[contains(text(),'Livingston')]");
 
         driver.findElement(By.xpath("//a[contains(text(),'Livingston')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")));
+        waiting("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']");
 
         driver.findElement(By.xpath("//li[contains(@class,'secondary-nav__item--first-non-crumb')]//a[@class='secondary-nav__link']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'St Johnstone')]")));
+        waiting("//span[contains(text(),'St Johnstone')]");
 
         boolean teamOneDisplay = driver.findElement(By.xpath("//span[@class='sp-c-fixture__team-name sp-c-fixture__team-name--away']//span[contains(text(),'Livingston')]")).getText().equals("Livingston");
         boolean teamTwoDisplay = driver.findElement(By.xpath("//span[contains(text(),'St Johnstone')]")).getText().equals("St Johnstone");
@@ -156,10 +151,10 @@ public class TestQuestionSubmit extends WebDriverSettings {
         boolean teamTwoScore = driver.findElement(By.xpath("//span[contains(@class,'sp-c-fixture__number--ft')][contains(text(),'0')]")).getText().equals("0");
 
         Assert.assertTrue((teamOneDisplay && teamTwoDisplay) && (teamOneScore && teamTwoScore));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/sport/football/51685367']")));
+        waiting("//a[@href='/sport/football/51685367']");
 
         driver.findElement(By.xpath("//a[@href='/sport/football/51685367']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']")));
+        waiting("//span[@class='fixture__number fixture__number--home fixture__number--ft']");
 
         String expectedScoreAberdeen = "1";
         WebElement actualScoreAberdeen = driver.findElement(By.xpath("//span[@class='fixture__number fixture__number--home fixture__number--ft']"));
--- src/test/java/webDriverSettings/WebDriverSettings.java
+++ src/test/java/webDriverSettings/WebDriverSettings.java
@@ -1,8 +1,12 @@
 package webDriverSettings;
 
 import org.junit.After;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,11 +15,16 @@ import java.util.concurrent.TimeUnit;
 public class WebDriverSettings {
 
     protected static WebDriver driver;
-
+    protected static WebDriverWait wait;
     @BeforeClass
     public static void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public void waiting(String xpath){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
     }
 
     @AfterClass
